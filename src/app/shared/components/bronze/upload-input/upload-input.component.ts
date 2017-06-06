304e8264d20eeaeda9969302b46b1cf8f5665f12
Honza Strapek <honstrap@gmail.com>
2017-06-06 12:47:32 +0200

    feat(upload-input): Created upload component (#37)

--- src/app/shared/components/bronze/upload-input/upload-input.component.ts
+++ src/app/shared/components/bronze/upload-input/upload-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'mss-upload-input',
@@ -6,4 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-input.component.scss']
 })
 export class UploadInputComponent {
+
+  /**
+   * sets accepted file types to see more visit: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+   * @type {string}
+   */
+  @Input() acceptFiles: string;
+  /**
+   * show label before input
+   * @type {string]
+   */
+  @Input() label: string;
+  /**
+   * switch between looks of input
+   * @type {string}
+   */
+  @Input() type: 'button' | 'input' = 'button';
+  /**
+   * output chosen file
+   * @type {EventEmitter<File>}
+   */
+  @Output() file = new EventEmitter<File>();
+
+  fileName: string;
+  id = Math.random().toString().substr(2);
+
+  onFileChange(event: Event): void {
+    const inputEvent = <HTMLInputElement>event.target;
+    if (this.isInputInvalid(inputEvent.files[0])) {
+      return;
+    }
+    this.fileName = inputEvent.files[0].name;
+    this.file.emit(inputEvent.files[0]);
+  }
+
+  isInputInvalid(file: File): boolean {
+    // TODO: add validation of files
+    return false;
+  }
 }
