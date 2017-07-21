833e6af17957b7e2d564df15963fe79500eb600c
PetrNavratil <petr.navratil93@gmail.com>
2017-07-21 12:31:27 +0200

    refactor(org-unit-list): adds filters (#134)

--- src/app/platform/org-unit-list/org-unit-list.component.ts
+++ src/app/platform/org-unit-list/org-unit-list.component.ts
@@ -7,7 +7,17 @@ import { ApiService } from '../../shared/services/api.service';
 import { RequestOptions } from '../../shared/models/pagination.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ListRouteParamsModel } from '../../shared/models/list-route-params.model';
-import { UnsubscribeSubject } from '../../shared/utils';
+import { MissingTokenResponse, UnsubscribeSubject } from '../../shared/utils';
+import { SelectItem } from '../../shared/components/bronze/select/select.component';
+import { OrgUnitFilterSections } from '../../shared/enums/org-unit-filter-section.enum';
+import { LanguageService } from '../../shared/services/language.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { StateModel } from '../../shared/models/state.model';
+import { ProfileModel } from '../../shared/models/profile.model';
+import { RoleService } from '../../shared/services/role.service';
+import { networkCodeActions } from '../../shared/reducers/network-code.reducer';
+import { merchantCodeActions } from '../../shared/reducers/merchant-code.reducer';
+import { CodeModel } from '../../shared/models/code.model';
 
 const ORG_UNIT_ENDPOINT = '/orgUnits';
 const ORG_UNIT_ROUTE = '/platform/org-units';
@@ -33,7 +43,21 @@ export class OrgUnitListComponent implements OnDestroy {
   private selectedOrgUnitId: string;
   private unsubscribe$ = new UnsubscribeSubject();
 
-  constructor(private store: Store<AppStateModel>, private api: ApiService, private router: Router, private route: ActivatedRoute) {
+  filterOptions: SelectItem[] = [];
+  visibleFilter: SelectItem;
+  OrgUnitFilterSections = OrgUnitFilterSections;
+  networkCodes: SelectItem[] = [];
+  merchantCodes: SelectItem[] = [];
+
+  filterForm: FormGroup;
+
+  constructor(private store: Store<AppStateModel>,
+              private api: ApiService,
+              private language: LanguageService,
+              private router: Router,
+              private fb: FormBuilder,
+              private roles: RoleService,
+              private route: ActivatedRoute) {
     this.store.select('orgUnitList').takeUntil(this.unsubscribe$).subscribe(
       (state: OrgUnitListState) => {
         if (state.error !== null) {
@@ -52,9 +76,102 @@ export class OrgUnitListComponent implements OnDestroy {
       (params: ListRouteParamsModel) => {
         this.pageNumber = Math.max(Number(params.page) || 0, 1);
         this.itemLimit = ITEM_LIMIT_OPTIONS.find(limit => limit === Number(params.limit)) || ITEM_LIMIT_OPTIONS[0];
-        this.store.dispatch({type: orgUnitListActions.ORG_UNIT_LIST_GET_REQUEST, payload: this.requestModel});
       }
     );
+
+    this.filterForm = this.fb.group({
+      code: [''],
+      merchantId: [{value: '', disabled: true}],
+      merchantName: [''],
+      name: [''],
+      networkCode: [{value: '', disabled: true}],
+      networkName: ['']
+    });
+
+    this.store.select('profile').takeUntil(this.unsubscribe$).subscribe(
+      (data: StateModel<ProfileModel>) => {
+        if (data.error) {
+          if (data.error instanceof MissingTokenResponse) {
+            return;
+          }
+          console.error('Profile API call has returned error', data.error);
+          return;
+        }
+        if (data.data && !data.loading /*because pn would be sad*/) {
+          const user = data.data;
+
+          this.roles.isVisible('filters.networkCodeSelect').subscribe(
+            networkResult => {
+              if (networkResult) {
+                this.store.dispatch({type: networkCodeActions.NETWORK_CODE_GET_REQUEST});
+              } else {
+                this.roles.isVisible('filters.merchantCodeSelect').subscribe(
+                  merchResult => {
+                    if (merchResult) {
+                      this.store.dispatch({type: merchantCodeActions.MERCHANT_CODE_GET_REQUEST, payload: user.resourceAcquirerId});
+                      this.filterOptions = Object.keys(OrgUnitFilterSections).filter(key => isNaN(Number(key)))
+                        .map(item => ({
+                          label: this.language.translate(`orgUnits.list.sections.${item}`),
+                          value: OrgUnitFilterSections[item]
+                        }));
+                    } else {
+                      this.filterOptions = [
+                        {
+                          label: this.language.translate(`orgUnits.list.sections.${OrgUnitFilterSections[OrgUnitFilterSections.BASIC]}`),
+                          value: OrgUnitFilterSections.BASIC
+                        }
+                      ];
+                    }
+                    this.visibleFilter = this.filterOptions[0];
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+
+    this.store.select('networkCodes').takeUntil(this.unsubscribe$).subscribe(
+      (data: StateModel<CodeModel[]>) => {
+        if (data.error) {
+          console.error('Error while getting network codes', data.error);
+          return;
+        }
+        if (data.data != null && !data.loading) {
+          this.networkCodes = data.data.map(item => ({label: item.code, value: item.id}));
+          this.filterForm.get('networkCode').enable();
+        }
+      }
+    );
+
+    this.store.select('merchantCodes').takeUntil(this.unsubscribe$).subscribe(
+      (data: StateModel<CodeModel[]>) => {
+        if (data.error) {
+          console.error('Error while getting merchant codes', data.error);
+          return;
+        }
+        if (data.data != null && !data.loading) {
+          this.merchantCodes = data.data.map(item => ({value: item.id}));
+          this.filterForm.get('merchantId').enable();
+        }
+      }
+    );
+
+    this.filterForm.get('networkCode').valueChanges.subscribe(
+      (code: string) => {
+        if (code && code.length > 0) {
+          this.store.dispatch({type: merchantCodeActions.MERCHANT_CODE_GET_REQUEST, payload: code});
+          this.merchantCodes = [];
+          this.filterForm.get('merchantId').patchValue('');
+          this.filterForm.get('merchantId').disable();
+        }
+      }
+    );
+  }
+
+  clearFilter(): void {
+    this.filterForm.reset();
   }
 
   ngOnDestroy(): void {
@@ -122,9 +239,14 @@ export class OrgUnitListComponent implements OnDestroy {
     this.router.navigate([`${ORG_UNIT_ROUTE}`, routeParams]);
   }
 
+  getOrgUnits(): void {
+    this.store.dispatch({type: orgUnitListActions.ORG_UNIT_LIST_GET_REQUEST, payload: this.requestModel});
+  }
+
   getSortedOrgUnits(sortInfo: any): void {
     this.sortOption = {predicate: sortInfo.sorts[0].prop, reverse: sortInfo.sorts[0].dir === 'asc'};
-    this.store.dispatch({type: orgUnitListActions.ORG_UNIT_LIST_GET_REQUEST, payload: this.requestModel});
+    this.getOrgUnits();
+
   }
 
   private get requestModel(): RequestOptions<OrgUnitPredicateObject> {
@@ -134,7 +256,7 @@ export class OrgUnitListComponent implements OnDestroy {
         numberOfPages: 0,
         start: (this.pageNumber - 1) * this.itemLimit
       },
-      search: {},
+      search: this.filterForm.value,
       sort: this.sortOption ? this.sortOption : {}
     };
   }
---
+++ src/app/shared/enums/org-unit-filter-section.enum.ts
@@ -0,0 +1,4 @@
+export enum OrgUnitFilterSections {
+  BASIC,
+  NETWORK
+}
